ef89f2713513c8af4b08550fa10b99003537c014
cortiz9888 <114939185+cortiz9888@users.noreply.github.com>
2022-10-20 16:13:32 -0500

    tic-tac-toe

---
+++ tic-tac-toe.js
@@ -0,0 +1,16 @@
+// i would like to get the board set up first
+// then move to making the blocks highlight 
+// make them clickable next
+// try to set the color for both sides or X and O would be ideal
+// then make the already clicked blocks, unclickable 
+// have the computer take the next turn
+// create win conditions (
+//   something like "if then then")
+// an alert that pops up once you win, lose or tie (would that also be another set of "if then then")
+// reset button after winning or losing or tying 
+// and a counter to keep track of the score (player: "#", computer: "#", ties:"#")
+// come on we got this!!!!
+// Questions: could we make the div blocks buttons that change color after every turn?
+
+let playGame = ['', '', '', '', '', '', '', '', '']
+
